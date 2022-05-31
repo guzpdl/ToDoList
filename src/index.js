28e254a325e48387e9e64c0c0e5f00b8331039a9
Guzman <guzpdl@gmail.com>
2022-05-31 17:40:47 -0300

    commit toDoList

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import { saludar } from './js/componentes';
+
+
+
 import './styles.css';
+import { TodoList } from './classes/index.js';
+import { Todo } from './classes/index.js';
+import { crearTodoHtml } from './js/componentes.js';
 
-const nombre = 'Guzman';
 
-saludar( nombre );
+export const todoList = new TodoList();
+
+todoList.todos.forEach(todo => crearTodoHtml(todo));
+
+
+
+
